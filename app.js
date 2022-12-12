bb6e64de78d6996692f4fed6dcaaf2e1e0c38877
Maarja <maarja-liis.kaldvee@voco.ee>
2022-12-12 09:52:06 +0200

    Delete single task from task list

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
 const form = document.querySelector('form')
+const taskList = document.querySelector('.collection')
+
 form.addEventListener('submit', addTask)
+taskList.addEventListener('click', deleteTask)
+
+function deleteTask(e){
+    if(e.target.textContent == 'X'){
+        if (confirm("Do you want to delete this task?")){
+            e.target.parentElement.remove()
+        }
+    }
+}
 
 function addTask(event) {
     const taskText = document.querySelector('#task').value
@@ -10,11 +21,17 @@ function addTask(event) {
     let liText = document.createTextNode(taskText)
     li.appendChild(liText)
 
+    const link = document.createElement('a')
+    link.setAttribute('href', '#')
+    link.appendChild(document.createTextNode('X'))
+
+
     let a = document.createElement('a')
     a.className = 'teal-text lighten-2 secondary-content'
     let aText = document.createTextNode('X')
     a.appendChild(aText)
     a.setAttribute('href', '#')
+    link.className = 'secondary-content'
 
     li.appendChild(a)
 
